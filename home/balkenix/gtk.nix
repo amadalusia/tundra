0392a326c30991112f91afdd5c9597d7b8a29251
Åsmund Balkesen <77496597+balkenix@users.noreply.github.com>
2024-05-22 13:44:47 +0100

    gtk: hacky fix 2

--- home/balkenix/gtk.nix
+++ home/balkenix/gtk.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: let
-  kanagawa-gtk-theme = pkgs.callPackage ../../pkgs/kanagawa-gtk-theme.nix {pkgs, ...};
+  kanagawa-gtk-theme = pkgs.callPackage ../../pkgs/kanagawa-gtk-theme.nix {inherit pkgs;};
 in {
   gtk = {
     enable = true;
