054a7f4ed50115b72d5f322a14ca3abcc480f510
Åsmund Balkesen <77496597+balkenix@users.noreply.github.com>
2024-05-22 21:45:33 +0100

    dconf: revert cleanup

--- home/balkenix/programs/de/gnome.nix
+++ home/balkenix/programs/de/gnome.nix
@@ -32,24 +32,22 @@
         activate-launcher = "['<Super>space']";
       };
     };
-    "org/gnome/desktop/wm" = {
-      preferences = {
-        workspace-names = ["Main" "Browsing" "Coding" "Messaging"];
-      };
-      keybindings = {
-        move-to-workspace-1 = "['<Super><Shift>1']";
-        switch-to-workspace-1 = "['<Super>1']";
-
-        move-to-workspace-2 = "['<Super><Shift>2']";
-        switch-to-workspace-2 = "['<Super>2']";
-
-        move-to-workspace-3 = "['<Super><Shift>3']";
-        switch-to-workspace-3 = "['<Super>3']";
-
-        move-to-workspace-4 = "['<Super><Shift>4']";
-        switch-to-workspace-4 = "['<Super>4']";
-      };
+    "org/gnome/desktop/wm/preferences" = {
+      workspace-names = ["Main" "Browsing" "Coding" "Messaging"];
     };
+    "org/gnome/desktop/wm/keybindings" = {
+      move-to-workspace-1 = "['<Super><Shift>1']";
+      switch-to-workspace-1 = "['<Super>1']";
+
+      move-to-workspace-2 = "['<Super><Shift>2']";
+      switch-to-workspace-2 = "['<Super>2']";
+
+      move-to-workspace-3 = "['<Super><Shift>3']";
+      switch-to-workspace-3 = "['<Super>3']";
+
+      move-to-workspace-4 = "['<Super><Shift>4']";
+      switch-to-workspace-4 = "['<Super>4']";
+     };
     "org/gnome/desktop/background" = {
       picture-uri = "${../../wallpapers/cafe.jpg}";
       picture-uri-dark = "${../../wallpapers/cafe.jpg}";
