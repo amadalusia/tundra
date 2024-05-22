d554c783a4b03d3ce36d8f8ba4876cf92f6f8f48
Åsmund Balkesen <77496597+balkenix@users.noreply.github.com>
2024-05-22 01:29:16 +0100

    nixos: change something around

--- hosts/snoland/configuration.nix
+++ hosts/snoland/configuration.nix
@@ -116,17 +116,14 @@
     useGlobalPkgs = true;
     useUserPackages = true;
     extraSpecialArgs = {inherit inputs outputs;};
-
     users.balkenix = import ../../home/balkenix;
   };
 
   programs.zsh.enable = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  
   environment.systemPackages = with pkgs; [
     git
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     wget
   ];
 
