ea48fc34a92a7f52e2256b8071444d9eef91ed34
Åsmund Balkesen <77496597+balkenix@users.noreply.github.com>
2024-05-23 18:38:38 +0100

    rofi: another oops

--- home/balkenix/programs/misc/rofi/default.nix
+++ home/balkenix/programs/misc/rofi/default.nix
@@ -1,8 +1,8 @@
 {pkgs, ...}: {
   programs.rofi = {
     enable = true;
-    package = pkgs.rofi-wayland
+    package = pkgs.rofi-wayland;
     extraConfig = builtins.readFile ./config/config.rasi;
-    theme = ./config/kanagawa.rasi
+    theme = ./config/kanagawa.rasi;
   };
 }
