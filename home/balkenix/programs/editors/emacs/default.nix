0cf235115baa4c6ec1e5deb2ca54174bbd153c64
Åsmund Balkesen <77496597+balkenix@users.noreply.github.com>
2024-05-24 18:06:35 +0100

    emacs: i realized i use use-package

--- home/balkenix/programs/editors/emacs/default.nix
+++ home/balkenix/programs/editors/emacs/default.nix
@@ -8,25 +8,13 @@
         use-package
         async
         avy
-        kanagawa-theme
-        doom-modeline
-        nix-mode
-        lsp-mode
-        lsp-ui
-        lsp-ivy
-        lsp-treemacs
-        which-key
-        dap-mode
-        helm-lsp
-        tree-sitter
-        magit
       ];
   };
 
   services.emacs = {
     enable = true;
     defaultEditor = true;
-    startWithUserSession = true;
+    startWithUserSession = "graphical";
     client.enable = false;
   };
 }
