4c9f92e337a7f59a5cd1ebdaca13a9b70db25ea5
Åsmund Balkesen <77496597+balkenix@users.noreply.github.com>
2024-05-24 23:39:27 +0100

    flake: update inputs (hm to 24.05)

--- hosts/snoland/configuration.nix
+++ hosts/snoland/configuration.nix
@@ -82,7 +82,7 @@
   hardware.opengl.enable = true;
   
   # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
   environment.gnome.excludePackages =
