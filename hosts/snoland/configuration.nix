4c32d914f6076084b04f8097eb596279f81aba65
Åsmund Balkesen <77496597+balkenix@users.noreply.github.com>
2024-05-31 01:00:43 +0100

    system: fuck gnome honestly

--- hosts/snoland/configuration.nix
+++ hosts/snoland/configuration.nix
@@ -45,20 +45,19 @@
     inputs.self.overlays.additions
   ];
 
-  systemd = {
-     user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-          Type = "simple";
-          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-          Restart = "on-failure";
-          RestartSec = 1;
-          TimeoutStopSec = 10;
-        };
-    };
+  security.polkit.enable = true;
+  systemd.user.services.polkit-kde-authentication-agent-1 = {
+    description = "polkit-kde-authentication-agent-1";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit-kde-agent}/libexec/polkit-kde-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
   };
 
 
@@ -97,7 +96,7 @@
 
   # enable OpenGL
   hardware.opengl.enable = true;
-
+  
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
