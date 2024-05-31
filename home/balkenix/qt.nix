2dc195cbb77117a63af7a5bb44bafb87486283b8
Åsmund Balkesen <77496597+balkenix@users.noreply.github.com>
2024-05-31 19:42:10 +0100

    home: configure kvantum to support my theme

--- home/balkenix/qt.nix
+++ home/balkenix/qt.nix
@@ -1,8 +1,21 @@
-_: {
+{config, pkgs, ...}: let
+  catppuccin-kvantum = pkgs.catppuccin-kvantum.override {
+    variant = "Macchiato";
+    accent = "Mauve";
+  };
+in {
   qt = {
     enable = true;
     style = {
       name = "Kvantum";
     };
+  };
+
+  xdg.configFile = {
+    "Kvantum/Catppuccin-Macchiato-Mauve".source = "${catppuccin-kvantum}/share/Kvantum/Catppuccin-Macchiato-Mauve";
+    "Kvantum/kvantum.kvconfig".text = ''
+      [General]
+      theme=Catppuccin-Macchiato-Mauve
+    '';
   };
 }
