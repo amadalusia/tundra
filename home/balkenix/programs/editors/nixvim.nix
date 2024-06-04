520e8c0dd512a440901cd3bee70e129082777c6a
Åsmund Balkesen <77496597+balkenix@users.noreply.github.com>
2024-06-04 15:39:30 +0100

    home: add nixvim file

---
+++ home/balkenix/programs/editors/nixvim.nix
@@ -0,0 +1,16 @@
+{ inputs, ... }: {
+  imports = [
+    inputs.nixvim.homeManagerModules.nixvim
+  ];
+
+  programs.nixvim = {
+    enable = true;
+    plugins = {
+      telescope = {
+        enable = true;
+      };
+    };
+  };
+  
+  stylix.targets.nixvim.enable = true;
+}
