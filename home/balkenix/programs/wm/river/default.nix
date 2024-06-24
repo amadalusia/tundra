93337e151b53bcce58133e40aac16f6b29f5d655
Åsmund Balkesen <balkenix@outlook.com>
2024-06-24 21:54:57 +0100

    home: change screenshot util and keybindings in river
    Signed-off-by: Åsmund Balkesen <balkenix@outlook.com>

--- home/balkenix/programs/wm/river/default.nix
+++ home/balkenix/programs/wm/river/default.nix
@@ -5,8 +5,7 @@
   ...
 }:
 let
-  screenshot = pkgs.callPackage ./scripts/screenshot.nix { };
-  screenshot-slurp = pkgs.callPackage ./scripts/screenshot-slurp.nix { };
+  screenshot_output = "${config.xdg.userDirs.pictures}/Screenshots/";
   river-bsp-layout = inputs.river-bsp-layout.packages.${pkgs.system}.default;
   colors = config.stylix.base16Scheme;
 in
@@ -15,7 +14,9 @@ in
     wl-clipboard
     libnotify
     pavucontrol
+    grimblast
     blueberry
+    networkmanagerapplet
   ];
 
   services.gpg-agent = {
@@ -63,24 +64,24 @@ in
         };
         map = {
           normal = {
-            "None Print" = "spawn '${screenshot-slurp}/bin/screenshot-slurp'";
-            "Shift Print" = "spawn '${screenshot}/bin/screenshot'";
+            "None Print" = "spawn '${pkgs.grimblast}/bin/grimblast --notify --freeze copysave area ${screenshot_output}'";
+            "Shift Print" = "spawn '${pkgs.grimblast}/bin/grimblast --notify copysave output ${screenshot_output}'";
             "${mod} Return" = "spawn '${config.programs.foot.package}/bin/foot'";
             "${mod} p" = "spawn '${config.programs.tofi.package}/bin/tofi-drun | ${pkgs.bash}/bin/bash'";
             "${mod}+Shift c" = "close";
             "${mod}+Shift q" = "exit";
-            "${mod} h" = "move left 100";
-            "${mod} j" = "move down 100";
-            "${mod} k" = "move up 100";
-            "${mod} l" = "move right 100";
+            "${mod} h" = "move left 96";
+            "${mod} j" = "move down 96";
+            "${mod} k" = "move up 96";
+            "${mod} l" = "move right 96";
             "${mod} t" = "default-layout bsp-layout";
             "${mod} f" = "default-layout";
             "${mod}+Shift Space" = "toggle-float";
             "${mod} m" = "toggle-fullscreen";
-            "${mod}+Shift h" = "resize horizontal -100";
-            "${mod}+Shift j" = "resize vertical -100";
-            "${mod}+Shift k" = "resize vertical 100";
-            "${mod}+Shift l" = "resize horizontal 100";
+            "${mod}+Shift h" = "resize horizontal -96";
+            "${mod}+Shift j" = "resize vertical -96";
+            "${mod}+Shift k" = "resize vertical 96";
+            "${mod}+Shift l" = "resize horizontal 96";
             "${mod} Tab" = "swap next";
             "${mod}+Shift Tab" = "swap previous";
           };
@@ -93,9 +94,10 @@ in
         };
         default-layout = "bsp-layout";
         spawn = [
-          "'${river-bsp-layout}/bin/river-bsp-layout --inner-gap 6 --outer-gap 6 --split-perc 0.5 &'"
+          "'${river-bsp-layout}/bin/river-bsp-layout --inner-gap 10 --outer-gap 10 --split-perc 0.5 &'"
           "'${config.services.hyprpaper.package}'"
           "'${config.programs.waybar.package}/bin/waybar'"
+          "'${pkgs.networkmanagerapplet}/bin/nm-applet'"
         ];
         border-color-focused = "0x${colors.base0B}";
         border-color-unfocused = "0x${colors.base03}";
