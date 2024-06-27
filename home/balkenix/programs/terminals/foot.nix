a57ac600d41d6ce29d90cc8d946be4005f62ba0e
Åsmund Balkesen <balkenix@outlook.com>
2024-06-27 09:01:11 +0100

    home: upgrade foot module to be coloured
    Signed-off-by: Åsmund Balkesen <balkenix@outlook.com>

--- home/balkenix/programs/terminals/foot.nix
+++ home/balkenix/programs/terminals/foot.nix
@@ -1,4 +1,8 @@
 { config, ... }:
+let
+  inherit (config) fonts;
+  inherit (config.colorScheme) palette;
+in
 {
   programs.foot = {
     enable = true;
@@ -7,7 +11,28 @@
       main = {
         term = "xterm-256color";
         pad = "10x10";
-        font = config.fonts.monospace.name;
+        font = "${fonts.monospace.name}:size=${toString fonts.monospace.size}";
+        dpi-aware = "no";
+      };
+      colors = {
+        background = "#${palette.base00}";
+        foreground = "#${palette.base05}";
+        regular0 = "${palette.base00}";
+        regular1 = "${palette.base01}";
+        regular2 = "${palette.base02}";
+        regular3 = "${palette.base03}";
+        regular4 = "${palette.base04}";
+        regular5 = "${palette.base05}";
+        regular6 = "${palette.base06}";
+        regular7 = "${palette.base07}";
+        bright0 = "${palette.base08}";
+        bright1 = "${palette.base09}";
+        bright2 = "${palette.base0A}";
+        bright3 = "${palette.base0B}";
+        bright4 = "${palette.base0C}";
+        bright5 = "${palette.base0D}";
+        bright6 = "${palette.base0E}";
+        bright7 = "${palette.base0F}";
       };
     };
   };
